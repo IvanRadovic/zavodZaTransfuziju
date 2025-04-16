7c85f1e479892fb2ce1ec4d4041a301690659130
Ivek <iradovic01@yahoo.com>
2025-04-16 12:48:27 +0200

    (PREBUILD): Fixing some small issue with prebuild

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import { Provider as PaperProvider } from 'react-native-paper';
